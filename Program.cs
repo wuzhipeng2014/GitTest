7055d2e564fa8ab37b8f273cc3673a01f967a3ff
WuZhipeng <798288416@qq.com>
2014-04-26 22:33:40 +0800

    clone后的第一次修改

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是我的第一次修改！");
+            Console.WriteLine("这是我的第二次修改！");
+            Console.WriteLine("已经是第三次修改了！");
+            Console.WriteLine("这是从github上克隆的版本，我已经修改过了");
+            Console.Read();
+        }
+    }
+}
