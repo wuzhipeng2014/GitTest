d8cd1d99a08336ab9979926b3b3ecb0347326d98
WuZhipeng <798288416@qq.com>
2014-04-27 10:08:20 +0800

    重新添加CTemp类

---
+++ CTemp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTest
+{
+    class CTemp
+    {
+        public CTemp() { }
+        public CTemp(string n, string i)
+        {
+            this.name = n;
+            this.id = i;
+        }
+        string name;
+        string id;
+
+    }
+}
